a0097a23740c57424869813884d140b67aaa48ce
ssl <1477152589@qq.com>
2022-10-31 00:43:34 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTst.java
+++ src/main/java/com/atguigu/git/GitTst.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTst {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
